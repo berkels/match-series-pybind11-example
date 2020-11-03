13df15a15721e3409651cc8261c00b1a6461f4bd
Benjamin Berkels <berkels@aices.rwth-aachen.de>
2020-11-03 18:48:49 +0100

    an example python binding for match-series

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,98 @@
 #include <pybind11/pybind11.h>
-
-int add(int i, int j) {
-    return i + j;
-}
+#include <pybind11/numpy.h>
+#include <iostream>
+#include <scalarArray.h>
+#include <configurators.h>
+#include <registration.h>
 
 namespace py = pybind11;
+
+template <typename DataType>
+class NumPy2DScalarArray : public qc::ScalarArray<DataType, qc::QC_2D> {
+public:
+    explicit NumPy2DScalarArray ( py::array_t<DataType, py::array::c_style> array )
+      : qc::ScalarArray<DataType, qc::QC_2D> ( array.shape(1), array.shape(0), const_cast<double*>(array.data()) )
+    {
+        if ( array.ndim() != 2 )
+            throw aol::Exception ( "Expected 2D NumPy array!\n", __FILE__, __LINE__ );
+    }
+};
+
+
+template <typename DataType>
+class NumPy2DMultiArray : public qc::MultiArray<DataType, qc::QC_2D> {
+public:
+    explicit NumPy2DMultiArray ( py::array_t<DataType, py::array::c_style> array )
+      : qc::MultiArray<DataType, qc::QC_2D> ( array.shape(2), array.shape(1), qc::MultiArray<DataType, qc::QC_2D>::CREATE_NO_ARRAYS )
+    {
+        if ( array.ndim() != 3 )
+            throw aol::Exception ( "Expected 3D NumPy array!\n", __FILE__, __LINE__ );
+
+        if ( array.shape(0) != 2 )
+            throw aol::Exception ( "Expected shape(0) to be 2!\n", __FILE__, __LINE__ );
+
+        const int numX = array.shape(2);
+        const int numY = array.shape(1);
+
+        for ( int i = 0; i < 2; ++i ) {
+            this->appendReference ( * ( new qc::ScalarArray<DataType, qc::QC_2D> ( numX, numY, const_cast<double*>(&(array.data()[i * numX * numY])), aol::FLAT_COPY ) ), true );
+        }
+    }
+};
+
+
+void array_test(py::array_t<double, py::array::c_style> array){
+    std::cerr << "Hello world\n";
+    std::cerr << array.itemsize();
+    const int dim = array.ndim();
+    for (int i = 0; i < dim; ++i)
+        std::cerr << array.shape(i);
+    double *data = const_cast<double*>(array.data());
+    for (int i = 0; i < array.size(); ++i)
+        std::cerr << data[i] << std::endl;
+
+    NumPy2DScalarArray<double> qcArray(array);
+
+    std::cerr << qcArray << std::endl;
+    qcArray[0, 0] = 666.;
+    std::cerr << qcArray << std::endl;
+    std::cerr << qcArray.norm() << std::endl;
+}
+
+
+py::array_t<double> register_images(py::array_t<double, py::array::c_style> Reference, py::array_t<double, py::array::c_style> Template, py::array_t<double, py::array::c_style> Deformation){
+  try {
+    NumPy2DScalarArray<double> referenceArray(Reference);
+    NumPy2DScalarArray<double> templateArray(Template);
+    NumPy2DMultiArray<double> deformationArray(Deformation);
+
+    qc::GridSize<qc::QC_2D> gridSize ( referenceArray );
+    typedef qc::RectangularGridConfigurator<double, qc::QC_2D, aol::GaussQuadrature<double, qc::QC_2D, 3> > ConfType;
+    const ConfType::InitType grid ( gridSize );
+    const double lambda = 10;
+
+    qc::NCCRegistrationConfigurator<ConfType> regisConf;
+    qc::DirichletRegularizationConfigurator<ConfType> regulConf ( grid );
+    qc::StandardRegistration<ConfType, ConfType::ArrayType, qc::NCCRegistrationConfigurator<ConfType>, qc::DirichletRegularizationConfigurator<ConfType> >
+      stdRegistration ( referenceArray, templateArray, regisConf, regulConf, lambda );
+
+    stdRegistration.findTransformation ( deformationArray );
+
+    qc::ScalarArray<double, qc::QC_2D> match ( grid );
+    qc::DeformImage<ConfType> ( templateArray, grid, match, deformationArray );
+
+
+    return py::array_t<double>(
+            {match.getNumY(), match.getNumX()}, // shape
+            {match.getNumX()*8, 8}, // C-style contiguous strides for double
+            match.getData()); // the data pointer
+  }
+  catch ( aol::Exception &el ) {
+    el.dump();
+  }
+  return py::array_t<double>();
+}
+
 
 PYBIND11_MODULE(cmake_example, m) {
     m.doc() = R"pbdoc(
@@ -16,21 +104,13 @@ PYBIND11_MODULE(cmake_example, m) {
         .. autosummary::
            :toctree: _generate
 
-           add
-           subtract
+           array_test
+           register_images
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
+    m.def("array_test", &array_test, "Hello World numpy array manipulation example");
+    m.def("register_images", &register_images, "A function which registers two numbers",
+          py::arg("reference_image"), py::arg("template_image"), py::arg("displacement"));
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,36 @@
-import cmake_example as m
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import cmake_example
+from scipy.ndimage import map_coordinates
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-assert m.__version__ == '0.0.1'
-assert m.add(1, 2) == 3
-assert m.subtract(1, 2) == -1
+a = np.array([[0.1, 10, 30], [15, 2, 13]])
+cmake_example.array_test(a)
+print(a)
+print(np.sqrt(np.sum(a**2)))
+
+reference_image = np.zeros((64, 64))
+template_image = np.zeros((64, 64))
+displacement = np.zeros((2, 64, 64))
+
+reference_image[10:30, 15:45] = 1
+template_image[25:45, 25:55] = 1
+
+out = cmake_example.register_images(reference_image, template_image, displacement)
+
+mpl.rcParams["image.cmap"] = 'gray'
+_, ax = plt.subplots(nrows=2, ncols=2)
+ax[0, 0].title.set_text('reference')
+ax[0, 0].imshow(reference_image)
+ax[0, 1].title.set_text('template')
+ax[0, 1].imshow(template_image)
+ax[1, 0].title.set_text('deformed template scipy')
+# Convert the displacement to a coordinate system
+coords = np.mgrid[0:template_image.shape[0], 0:template_image.shape[1]] \
+         + np.multiply([displacement[1, ...], displacement[0, ...]], (np.max(template_image.shape)-1))
+ax[1, 0].imshow(map_coordinates(template_image, coords, order=1, mode='nearest', cval=0.0))
+ax[1, 1].title.set_text('deformed template returned')
+ax[1, 1].imshow((out))
+plt.show()
